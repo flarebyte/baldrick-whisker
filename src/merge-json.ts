20f06167389ff92d330f715a9b35100fd125363a
Olivier Huin <olih@users.noreply.github.com>
2022-05-31 09:27:07 +0100

    Add draft for merging

--- src/merge-json.ts
+++ src/merge-json.ts
@@ -1,3 +1,18 @@
+import { InputContent, JsonContent } from "./model";
+
+const mergeTwoContents = (a: JsonContent, b: JsonContent): JsonContent => {
+    const tupleAList = Object.entries(a)
+    const tupleBList = Object.entries(b)
+    const results = [...tupleAList]
+    
+    return Object.fromEntries(results);
+
+}
+
 export const mergeContents = (inputContents: InputContent[]): JsonContent => {
-    const aa = inputContents
+    let result: JsonContent = {};
+    for (const content of inputContents) {
+        
+    }
+    return result;
 }
